f5bf6e2666ea2592bb1c3464b233a2a16f5f88d0
everton pereira <vilmamariadasilvaaa@gmail.com>
2024-03-28 09:34:58 -0300

    segundo commit do JS

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let conta = 0
 let pessoas = 0
+let porcentagem = 0
 
 const containput = document.querySelector("#conta")
 containput.addEventListener("input", recebervalorconta )
@@ -26,4 +27,13 @@ function receberquantidadepessoas(evento)  {
     divErro.setAttribute("id", "")
     pessoas = Number(evento.target.value)
   }
+}
+
+const botoesGorjeta = document.querySelectorAll (".gorjet input [type='number']")
+botoesGorjeta.forEach(botao => {
+     botao.addEventListener("click", receberPorcentagem)
+})
+
+function receberPorcentagem(evento) {
+    console.log(evento.target.value)
 }
